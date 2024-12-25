4cc7e14f077a3db1d17a3e8d4eb8fd2bd9722004
Charles Caleb <charlesofficial9@gmail.com>
2024-12-25 16:10:32 +0100

    update

--- src/hooks/images/useUploadImage.ts
+++ src/hooks/images/useUploadImage.ts
@@ -1,5 +1,6 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { uploadImageApi } from "../../services/apiUploadImage";
+import toast from "react-hot-toast";
 
 export function useUploadImage(headers: Record<string, string>) {
   const queryClient = useQueryClient();
@@ -14,10 +15,12 @@ export function useUploadImage(headers: Record<string, string>) {
       return uploadImageApi(formData, headers);
     },
     onSuccess: () => {
+      toast.success("Image upload successfull");
       // Invalidate user query to refetch data
       queryClient.invalidateQueries(["user"] as any);
     },
     onError: () => {
+      toast.error("Error uploading image");
       console.error("An error occurred while uploading the file.");
     },
   });
