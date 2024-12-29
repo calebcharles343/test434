ae58d46ca58dd62efc726a3390387fec96014c06
Charles Caleb <charlesofficial9@gmail.com>
2024-12-29 21:21:24 +0100

    update

--- src/ui/Sidebar.tsx
+++ src/ui/Sidebar.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { useLogout } from "../features/authentication/useLogout.ts";
 import SpinnerMini from "./SpinnerMini.tsx";
 import sideBarImg2 from "../data/img/SIdeBar-2.jpg";
@@ -18,6 +18,7 @@ const Sidebar: React.FC = () => {
   const [user, setUser] = useState(sessionStorageUser());
   const isAuthenticated = !!user;
   const { logout, isPending } = useLogout();
+  const location = useLocation();
 
   const handleLogout = async () => {
     logout();
@@ -35,6 +36,10 @@ const Sidebar: React.FC = () => {
     };
   }, []);
 
+  const isActive = (path: string) => {
+    return location.pathname === path ? "bg-[#ffa82b] text-gray-800" : "";
+  };
+
   return (
     <div>
       <nav
@@ -50,24 +55,30 @@ const Sidebar: React.FC = () => {
         <ul className="flex flex-col item gap-2 w-full px-6 md:px-8 flex-grow">
           <li>
             <Link
-              to="home"
-              className="flex items-center p-2 rounded hover:bg-[#ffa82b] hover:text-gray-800 transition-colors duration-200"
+              to="/home"
+              className={`flex items-center p-2 rounded hover:bg-[#ffa82b] hover:text-gray-800 transition-colors duration-200 ${isActive(
+                "/home"
+              )}`}
             >
               <BiHome className="mr-2" /> Home
             </Link>
           </li>
           <li>
             <Link
-              to="cartPage"
-              className="flex items-center p-2 rounded hover:bg-[#ffa82b] hover:text-gray-800 transition-colors duration-200"
+              to="/cartPage"
+              className={`flex items-center p-2 rounded hover:bg-[#ffa82b] hover:text-gray-800 transition-colors duration-200 ${isActive(
+                "/cartPage"
+              )}`}
             >
               <BiCart className="mr-2" /> Cart
             </Link>
           </li>
           <li>
             <Link
-              to="home/product/:id"
-              className="flex items-center p-2 rounded hover:bg-[#ffa82b] hover:text-gray-800 transition-colors duration-200"
+              to="/home/product/:id"
+              className={`flex items-center p-2 rounded hover:bg-[#ffa82b] hover:text-gray-800 transition-colors duration-200 ${isActive(
+                "/home/product/:id"
+              )}`}
             >
               <BiInfoCircle className="mr-2" /> Product info
             </Link>
@@ -75,8 +86,10 @@ const Sidebar: React.FC = () => {
           {isAuthenticated && user.role !== "Admin" && (
             <li>
               <Link
-                to="orders"
-                className="flex items-center p-2 rounded hover:bg-[#ffa82b] hover:text-gray-800 transition-colors duration-200"
+                to="/orders"
+                className={`flex items-center p-2 rounded hover:bg-[#ffa82b] hover:text-gray-800 transition-colors duration-200 ${isActive(
+                  "/orders"
+                )}`}
               >
                 <FiMenu className="mr-2" /> Orders
               </Link>
@@ -85,8 +98,10 @@ const Sidebar: React.FC = () => {
           {isAuthenticated && user.role === "Admin" && (
             <li>
               <Link
-                to="adminOrders"
-                className="flex items-center p-2 rounded hover:bg-[#ffa82b] hover:text-gray-800 transition-colors duration-200"
+                to="/adminOrders"
+                className={`flex items-center p-2 rounded hover:bg-[#ffa82b] hover:text-gray-800 transition-colors duration-200 ${isActive(
+                  "/adminOrders"
+                )}`}
               >
                 <FiMenu className="mr-2" /> Admin Orders
               </Link>
@@ -113,7 +128,7 @@ const Sidebar: React.FC = () => {
           )
         ) : (
           <Link
-            to="auth"
+            to="/auth"
             className="flex items-center justify-center gap-2 mt-auto mb-0 text-gray-800 bg-gray-50 p-2 rounded hover:bg-[#ffa82b] hover:text-gray-800 transition-colors duration-200"
           >
             <BiLogIn />
