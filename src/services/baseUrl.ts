2d15b5203051053040aa4d1e2ca58068c32dece4
Charles Caleb <charlesofficial9@gmail.com>
2025-01-01 23:10:52 +0100

    update

--- src/services/baseUrl.ts
+++ src/services/baseUrl.ts
@@ -1,4 +1,5 @@
 export const baseUrl = () => {
-  const url = "https://tia-backend-final.onrender.com/api/v1/e-commerce";
+  // const url = "https://tia-backend-final.onrender.com/api/v1/e-commerce";
+  const url = "http://127.0.0.1:5002/api/v1/e-commerce";
   return url;
 };
